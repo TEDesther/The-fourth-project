7694024026dacad92425015eab7f2e1ede17497b
Tobi Esther <daviid.tobbyesther@gmail.com>
2024-05-16 11:03:30 +0100

    first commit

---
+++ slice.py
@@ -0,0 +1,34 @@
+Data=[["Student","Course","Level","Gender"],
+ ["Tobi","Mechanical",400,"Male"], 
+["Frank","Electrical","","Female"],
+["Emmanuel","Chemistry",200,""],
+["John","Petroleum",100,"Male"],
+["Jessie","",300,"Female"],
+["Opus","Law",500,""],
+["Francis","Agric","",""]]
+
+
+# data2=[Data[0:2]]
+data = Data[1:]
+# print(len(data))
+
+Data.reverse()
+
+# print(Data)
+
+
+# for i in range(-5,-2):
+#     print(i)
+
+L = list(range(4,10))
+
+# print(L)
+
+
+a = ["mango","orange"]
+b = ["food","car"]
+c= ["rice","beans"]
+
+d =  a + b + c
+
+print(d)
---
+++ slice_testground.py
@@ -0,0 +1,60 @@
+data = ["dog",2,3,4,5,6,7,8,9,"mango","carrot","yaba","kogi"]
+
+# 2,3,4
+#mango caroot
+#yaba,kogi
+# dog,2,3,4,5
+#5,6,7,8,9,...,kogif
+
+
+# print(data[1:4])
+# print(data[-4:-2])
+# print(data[-2:])
+# print(data[0:5])
+# print(data[4:])
+
+
+
+# data =[11,12,13,14,15,16]
+
+# print(data[0:2])
+
+# print(data[1:4])
+
+# print(data[1:-1])
+
+# print(data[3:])
+
+# print(data[:-1])
+
+# print(data[:])
+
+L=["car","boat","ship"]
+
+L.insert(2,"airplane")
+
+L.append("car")
+L.append("boat")
+
+# print(L)
+
+# print(L.index("ship"))
+
+print(L.count("car"))
+
+print(L)
+
+L.sort()
+
+print(L)
+
+L.reverse()
+print(L)
+
+L.pop(0)
+
+print(L)
+
+L.remove("boat")
+
+print(L)
